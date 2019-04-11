e641d5960fcb23ccb8d1a4b966cd73224d99e066
Bianca Dorta <30930818+biancadorta@users.noreply.github.com>
2019-04-11 11:14:34 -0300

    Add WebServiceRest e pdf Apostila

--- JavaRest/WSRestServidor/src/java/br/unicamp/cotuca/Service.java
+++ JavaRest/WSRestServidor/src/java/br/unicamp/cotuca/Service.java
@@ -28,49 +28,12 @@ import javax.ws.rs.core.MediaType;
 @Path("generic")
 public class Service {
     
-    private static ArrayList<Aluno> listaAluno = 
-                           new ArrayList<Aluno>();
-
-    @Context
-    private UriInfo context;
-
-    /**
+     /**
      * Creates a new instance of Service
      */
     public Service() {
         
-        listaAluno.clear();
-        
-        Aluno a1 = new Aluno();
-        a1.setId(1);
-        a1.setNome("João");
-        a1.setCurso("Ciência da Computação");
-        
-        Aluno a2 = new Aluno();
-        a2.setId(2);
-        a2.setNome("Prato");
-        a2.setCurso("Ciência do Esporte");
-        
-        Aluno a3 = new Aluno();
-        a3.setId(3);
-        a3.setNome("Fábio");
-        a3.setCurso("Engenharia da Pesca");
-        
-        Aluno a4 = new Aluno();
-        a4.setId(4);
-        a4.setNome("Kaio");
-        a4.setCurso("Artes Plástica Litorânea");
-        
-        Aluno a5 = new Aluno();
-        a5.setId(5);
-        a5.setNome("Terra");
-        a5.setCurso("Artes Plástica Litorânea");
-        
-        listaAluno.add(a1);
-        listaAluno.add(a2);
-        listaAluno.add(a3);
-        listaAluno.add(a4);
-        listaAluno.add(a5);
+  
     }
 
     /**
@@ -93,105 +56,4 @@ public class Service {
     public void putJson(String content) {
     }
     
-    /**
-     *
-     * @return
-     */
-    @GET
-    @Path("/consulta")
-    @Produces(MediaType.APPLICATION_JSON)
-    public ArrayList<Aluno> consultaAluno(){
-        
-        return listaAluno;
-        
-    }
-    
-    @GET
-    @Path("consultaId/{Id}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    public Aluno consultaIdAluno(@PathParam("Id")int id){
-        
-        ArrayList<Aluno> listaTudo = consultaAluno();
-        
-        Aluno resultado = null;
-        
-        for (Aluno a: listaTudo){
-            if(a.getId()== id)
-            {
-                resultado = a;
-                break;
-            }
-        }
-        
-        return resultado;        
-    }
-    
-    @GET
-    @Path("consultaCurso/{Curso}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    public ArrayList<Aluno> consultaCursoAluno(@PathParam("Curso")String curso){
-        
-         ArrayList<Aluno> listaTudo = consultaAluno();
-         
-         ArrayList<Aluno> resultado = new ArrayList<Aluno>();   
-         
-         
-         
-         for(Aluno a : listaTudo){
-            if(a.getCurso().equals(curso))
-            {
-               resultado.add(a);                
-            }
-         }        
-         
-         return resultado;
-        
-    } 
-    
-    
-    
-    @POST
-    @Path("/incluirAluno")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    public ArrayList<Aluno> incluiAluno(Aluno aluno)
-    {        
-        listaAluno.add(aluno);
-        return listaAluno;        
-    }
-    
-    @PUT
-    @Path("/alterarAluno")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public ArrayList<Aluno> alterarAluno (Aluno aluno){        
-       // ArrayList<Aluno> listaTudo = listaAluno;        
-        for(Aluno a: listaAluno ){
-            if(a.getId() == aluno.getId()){
-                a.setNome(aluno.getNome());
-                a.setCurso(aluno.getCurso());
-               
-            }              
-        }
-        return listaAluno;        
-    }   
-    
-    @GET
-    @Path("/excluirAluno/{id}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    
-    public ArrayList<Aluno> excluirAluno(@PathParam("id") Integer id) throws Exception{        
-        
-        ArrayList<Aluno> listaTudo = consultaAluno(); 
-        Aluno resultado = null;
-        for(Aluno a: listaTudo){
-            if(a.getId()== id){
-                resultado = a;
-                listaTudo.remove(resultado);
-                break;
-            }
-        }        
-        return listaTudo;
-    }
 }
