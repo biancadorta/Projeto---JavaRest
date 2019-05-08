44aabd42c651a16afe38a8cf4893b1290df8d38e
Emmanuel <30930846+EmmanuelCasangel@users.noreply.github.com>
2019-05-08 13:55:00 -0300

    a

---
+++ ProjetoJavaRest(Simone)/src/java/bd/BDSQLServer.java
@@ -0,0 +1,30 @@
+package bd;
+
+import bd.core.*;
+import br.unicamp.cotuca.bd.daos.*;
+
+public class BDSQLServer
+{
+    public static final MeuPreparedStatement COMANDO;
+
+    static
+    {
+    	MeuPreparedStatement comando = null;
+
+    	try
+        {
+            comando =
+            new MeuPreparedStatement (
+            "com.microsoft.sqlserver.jdbc.SQLServerDriver",
+            "jdbc:sqlserver://regulus:1433;databasename=BD17165",
+            "BD17165", "BD17165");
+        }
+        catch (Exception erro)
+        {
+            System.err.println ("Problemas de conexao com o BD");
+            System.exit(0); // aborta o programa
+        }
+        
+        COMANDO = comando;
+    }
+}
---
+++ ProjetoJavaRest(Simone)/src/java/bd/dbos/Aluno.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bd.dbos;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author u17165
+ */
+public class Aluno implements Cloneable{   
+    
+    private String ra;
+    private String nome;
+    private String email; 
+    
+    public Aluno(String ra, String nome, String email) {
+        this.ra = ra;
+        this.nome = nome;
+        this.email = email;
+    }   
+
+    public String getRa() {
+        return ra;
+    }
+
+    public void setRa(String ra) {
+        this.ra = ra;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    } 
+    
+      @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 97 * hash + Objects.hashCode(this.ra);
+        hash = 97 * hash + Objects.hashCode(this.nome);
+        hash = 97 * hash + Objects.hashCode(this.email);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Aluno other = (Aluno) obj;
+        if (!Objects.equals(this.ra, other.ra)) {
+            return false;
+        }
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        if (!Objects.equals(this.email, other.email)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Aluno{" + "ra=" + ra + ", nome=" + nome + ", email=" + email + '}';
+    }
+}
