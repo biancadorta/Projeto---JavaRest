a3774f5bff0925a4a785f8bfd74daf830608ad44
BIANCA FERREIRA DORTA <u17165@academico.cotuca.unicamp.br>
2019-05-09 10:35:49 -0300

    aaa

--- ClienteREST/src/main/java/ClientRest.java
+++ ClienteREST/src/main/java/ClientRest.java
@@ -20,21 +20,33 @@ public class ClientRest {
         String urlDeletarPorRa = "http://localhost:8080/WebService/webresources/generic/excluirAluno/{ra}";
         String urlIncluir = "http://localhost:8080/WebService/webresources/generic/incluirAluno";
 
-        String getAlunos = cliente.getAlunos(urlGetAlunos);
-        String getAlunoByRa = cliente.getAlunoByRa(urlGetAlunoPorRa);
-        String getAlunoByNome = cliente.getAlunoByNome(urlGetAlunoPorNome);
+        //String getAlunos = cliente.getAlunos(urlGetAlunos);
+        //String getAlunoByRa = cliente.getAlunoByRa(urlGetAlunoPorRa);
+        //String getAlunoByNome = cliente.getAlunoByNome(urlGetAlunoPorNome);
         String alterar = cliente.alterar(urlAlterar);
         String deletar = cliente.deletarPorRa( urlDeletarPorRa);
         String incluir = cliente.incluir(urlIncluir);
         
         //Mostra no formato json
-        System.out.println(getAlunos);
-        System.out.println(getAlunoByRa);
-        System.out.println(getAlunoByNome);
+        //System.out.println(getAlunos);
+        //System.out.println(getAlunoByRa);
+        //System.out.println(getAlunoByNome);
         System.out.println(incluir);
         System.out.println(alterar);
-        System.out.println(deletar);
+        System.out.println(deletar);        
+    }
+
+    public String alterar(String urlAlterar){
         
     }
+
+    public String deletarPorRa(String urlDeletarPorRa) {
+       
+    }
+
+    public String incluir(String urlIncluir) {
+        
+    }
+    
     
 }
