fa50d1eba382bdfd20ad20dd2606157354b375bf
Emmanuel <30930846+EmmanuelCasangel@users.noreply.github.com>
2019-05-09 10:37:05 -0300

    a

---
+++ ClienteREST/src/main/java/clienterest/ClientRest.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clienterest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ *
+ * @author u17172
+ */
+public class ClientRest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+        
+        
+         ClientRest cliente = new ClientRest();
+        
+        String urlGetAlunos = "http://localhost:8080/WebService/webresources/generic/consulta";
+        String urlGetAlunoPorRa  = "http://localhost:8080/WebService/webresources/generic/consultaRa/{ra}";
+        String urlGetAlunoPorNome = "http://localhost:8080/WebService/webresources/generic/consultaNome/{nome}";
+        String urlAlterar = "http://localhost:8080/WebService/webresources/generic/alterar";
+        String urlDeletarPorRa = "http://localhost:8080/WebService/webresources/generic/excluirAluno/{ra}";
+        String urlIncluir = "http://localhost:8080/WebService/webresources/generic/incluirAluno";
+
+        String getAlunos = cliente.getAlunos(urlGetAlunos);
+        /*String getAlunoByRa = cliente.getAlunoByRa(urlGetAlunoPorRa);
+        String getAlunoByNome = cliente.getAlunoByNome(urlGetAlunoPorNome);
+        String alterar = cliente.alterar(urlAlterar);
+        String deletar = cliente.deletarPorRa( urlDeletarPorRa);
+        String incluir = cliente.incluir(urlIncluir);*/
+        
+        //Mostra no formato json
+        System.out.println(getAlunos);
+      /*  System.out.println(getAlunoByRa);
+        System.out.println(getAlunoByNome);
+        System.out.println(incluir);
+        System.out.println(alterar);
+        System.out.println(deletar);*/
+        
+    }
+    
+       public String getAlunos(String url) throws MalformedURLException, IOException{
+        
+        URL objURL = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) objURL.openConnection();
+        
+        con.setRequestMethod("GET");
+        int responseCode = con.getResponseCode();
+        
+        System.out.println("\nEnviando requisição 'GET' para URL: " + url);
+        
+        System.out.println("Response Code: "+ responseCode);
+        
+        BufferedReader br = 
+             new BufferedReader(new InputStreamReader(con.getInputStream()));
+        
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+        
+        while((inputLine = br.readLine()) != null){
+            response.append(inputLine);
+        }
+        
+        br.close();
+        con.disconnect();
+        
+        return response.toString();
+    }
+    
+    
+}
